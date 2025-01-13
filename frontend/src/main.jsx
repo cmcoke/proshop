5514b1ff29694255bbbfb7698b37210ca368c273
cmcoke <minoru.cm@gmail.com>
2025-01-13 11:13:21 -0500

    Created the ProductScreen.jsx file for displaying an individual product.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,6 +11,7 @@ import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
 import App from './App.jsx';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 // The router configuration is created using the `createBrowserRouter` function.
 const router = createBrowserRouter(
@@ -20,7 +21,8 @@ const router = createBrowserRouter(
 
     // Route path='/' element={<App />} creates a route that renders the `App` component when the URL matches `/`.
     <Route path='/' element={<App />}>
-      <Route index={true} path='/' element={<HomeScreen />} /> {/* creates a route that renders the `HomeScreen` component when the URL matches `/`.  */}
+      <Route index={true} path='/' element={<HomeScreen />} /> {/* creates a route that renders the `HomeScreen` component when the URL matches `/`  */}
+      <Route path='/product/:id' element={<ProductScreen />} /> {/* creates a route that renders the `ProductScreen` component when the URL matches `/product/:id`. */}
     </Route>
 
   )
