6eb08a409611ce21284450696b4ee7b77fce41d8
cmcoke <minoru.cm@gmail.com>
2025-01-13 10:16:47 -0500

    Implemented react-router-dom to handle routing

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
 
 const Header = () => {
@@ -7,17 +8,17 @@ const Header = () => {
     <header>
       <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>
+          <Navbar.Brand as={Link} to='/'>
             <img src={logo} alt='ProShop' />
             ProShop
           </Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-              <Nav.Link href='/cart'>
+              <Nav.Link as={Link} to='/cart'>
                 <FaShoppingCart /> Cart
               </Nav.Link>
-              <Nav.Link href='/login'>
+              <Nav.Link as={Link} to='/login'>
                 <FaUser /> Sign In
               </Nav.Link>
             </Nav>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,33 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 // import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
 import App from './App.jsx';
+import HomeScreen from './screens/HomeScreen';
+
+// The router configuration is created using the `createBrowserRouter` function.
+const router = createBrowserRouter(
+
+  // The `createRoutesFromElements` function creates a route configuration from JSX elements.
+  createRoutesFromElements(
+
+    // Route path='/' element={<App />} creates a route that renders the `App` component when the URL matches `/`.
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />} /> {/* creates a route that renders the `HomeScreen` component when the URL matches `/`.  */}
+    </Route>
+
+  )
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} /> {/* The `RouterProvider` component provides the router configuration to the rest of the application. */}
   </StrictMode>,
 );
